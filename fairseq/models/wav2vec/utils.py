df7fa0895ff6a1eeeca22de57f6fa8c758dd75ba
Phanidhar Chinchili <phanich@qti.qualcomm.com>
2024-12-03 19:05:21 -0800

    changes

--- fairseq/models/wav2vec/utils.py
+++ fairseq/models/wav2vec/utils.py
@@ -4,7 +4,21 @@
 # LICENSE file in the root directory of this source tree.
 
 import math
+import torch
 import torch.nn.functional as F
+
+# def pad_to_multiple(x, multiple, dim=-1, value=0):
+#     # Inspired from https://github.com/lucidrains/local-attention/blob/master/local_attention/local_attention.py#L41
+#     if x is None:
+#         return None, 0
+#     tsz = x.size(dim)
+#     m = tsz / multiple
+#     remainder = math.ceil(m) * multiple - tsz
+#     if m.is_integer():
+#         return x, 0
+#     pad_offset = (0,) * (-1 - dim) * 2
+
+#     return F.pad(x, (*pad_offset, 0, remainder), value=value), remainder
 
 
 def pad_to_multiple(x, multiple, dim=-1, value=0):
@@ -14,7 +28,7 @@ def pad_to_multiple(x, multiple, dim=-1, value=0):
     tsz = x.size(dim)
     m = tsz / multiple
     remainder = math.ceil(m) * multiple - tsz
-    if m.is_integer():
+    if m == math.floor(m):  # Check if m is an integer
         return x, 0
     pad_offset = (0,) * (-1 - dim) * 2
 
